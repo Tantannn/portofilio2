8a8dda728b49b8ba1228b317c4b566a4fe126962
Tantannn <tango.11@yahoo.com>
2023-07-24 15:30:30 +0700

    nav

--- src/Component/Navbar/DropdownMenu/DropdownMenu.tsx
+++ src/Component/Navbar/DropdownMenu/DropdownMenu.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
 import "./DropdownMenu.css";
 
 export default function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = React.useState("main");
+  const [menuHeight, setMenuHeight] = React.useState(100);
+function calcHeight(el : any) {
+  const height = el.offsetHeight;
+    setMenuHeight(height);
+}
+
   const DropdownItem = (props: any) => {
     return (
-      <a href="#" className="menu-item">
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
         <span className="icon-button">{props.leftIcon}</span>
         {props.children}
         <span className="icon-right">{props.rightIcon}</span>
@@ -12,9 +24,41 @@ export default function DropdownMenu() {
     );
   };
   return (
-    <div className="dropdown">
-      <DropdownItem>My Profile</DropdownItem>
-      <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+    <div className="dropdown" style={{height: menuHeight}}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        unmountOnExit
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)" goToMenu="setting">
+            Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "setting"}
+        timeout={300}
+        unmountOnExit
+        classNames="menu-secondary"
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+          <DropdownItem leftIcon=":)" rightIcon=":)"></DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
