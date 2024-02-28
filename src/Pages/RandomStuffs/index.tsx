fb4a1f7264dfc954aaa1cfc80dce21e3f1a2113d
Tantannn <tango.11@yahoo.com>
2024-02-29 00:25:30 +0700

    aaa

--- src/Pages/RandomStuffs/index.tsx
+++ src/Pages/RandomStuffs/index.tsx
@@ -2,7 +2,8 @@ import "./RandomStuffs.css";
 import React, { useState } from 'react';
 
 export default function RandomStuffs() {
-    const [phoneNumber, setPhoneNumber] = useState(0);
+    const [phoneNumber, setPhoneNumber] = useState<number>(0);
+    const [advanceMode, setAdvanceMode] = useState(false);
 
     const handlePhoneNumberChange = (e : any) => {
         setPhoneNumber(e.target.value);
@@ -13,19 +14,33 @@ export default function RandomStuffs() {
         return  strNum.slice(0, 2) + '-' + strNum.slice(2);
     };
 
+    const confirmPhoneNumber = () => {
+        const text = 'Are your sure this is your number?'
+        if (window.confirm(text) === true) {
+            alert('Your number is ' + formatNumber(phoneNumber));
+        }
+    }
+
+        // <div className=`phone-number-changer ${phoneNumber && 'phone-number-changer-advance}`>
     return (
-        <div className="volume-changer">
-            <h4>Pealse enter Your Phone Number</h4>
-            <input
-                type="range"
-                min="00000000000"
-                max="99999999999"
-                value={phoneNumber}
-                onChange={handlePhoneNumberChange}
-            />
-            <p>Volume: {formatNumber(phoneNumber)}</p>
-            <input type="checkbox" id="checkbox" onChange={() => {}}/> 
-            <label>Advance Mode</label>
-        </div>
+        <>
+            <div >
+                <h4>Pealse enter Your Phone Number</h4>
+                <input
+                    className={`phone-number-changer ${advanceMode && 'phone-number-changer-advance'}`}
+                    type="range"
+                    min="00000000000"
+                    max="99999999999"
+                    value={phoneNumber}
+                    onChange={handlePhoneNumberChange}
+                />
+                <p>Your phone number: {formatNumber(phoneNumber)}</p>
+                <input type="checkbox" id="checkbox" onChange={() => setAdvanceMode(!advanceMode)}/> 
+                <label>Advance Mode</label>
+                <input type="checkbox" id="checkbox" onChange={() => setAdvanceMode(!advanceMode)}/> 
+                <label>Enhance</label>
+            </div>
+            <button className="" onClick={() => confirmPhoneNumber()}>Submit</button>
+        </>
     );
 }
