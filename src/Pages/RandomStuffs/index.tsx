46f3350951b7d2cd9043752ba5d0f42c257fde8d
Tantannn <tango.11@yahoo.com>
2024-03-02 14:27:43 +0700

    something

--- src/Pages/RandomStuffs/index.tsx
+++ src/Pages/RandomStuffs/index.tsx
@@ -1,33 +1,40 @@
 import "./RandomStuffs.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function RandomStuffs() {
     const [phoneNumber, setPhoneNumber] = useState<number>(0);
+    const [enhance, setEnhance] = useState(false);
     const [advanceMode, setAdvanceMode] = useState(false);
 
-    const handlePhoneNumberChange = (e : any) => {
+    const rotation = (phoneNumber * 180) / 100000000000;
+
+    const handlePhoneNumberChange = (e: any) => {
         setPhoneNumber(e.target.value);
     };
 
-    const formatNumber = (num : number) => {
-        let strNum = num.toString().padStart(11, '0');
-        return  strNum.slice(0, 2) + '-' + strNum.slice(2);
+    const formatNumber = (num: number) => {
+        let strNum = num.toString().padStart(11, "0");
+        return strNum.slice(0, 2) + "-" + strNum.slice(2);
     };
 
     const confirmPhoneNumber = () => {
-        const text = 'Are your sure this is your number?'
+        const text = "Are your sure this is your number?";
         if (window.confirm(text) === true) {
-            alert('Your number is ' + formatNumber(phoneNumber));
+            alert("Your number is " + formatNumber(phoneNumber));
         }
-    }
+    };
 
-        // <div className=`phone-number-changer ${phoneNumber && 'phone-number-changer-advance}`>
     return (
         <>
-            <div >
+            <div>
                 <h4>Pealse enter Your Phone Number</h4>
                 <input
-                    className={`phone-number-changer ${advanceMode && 'phone-number-changer-advance'}`}
+                    className={`phone-number-changer ${
+                        advanceMode && "phone-number-changer-advance"
+                    }`}
+                    style={{
+                        transform: enhance ? `rotate(${rotation}deg)` : "",
+                    }}
                     type="range"
                     min="00000000000"
                     max="99999999999"
@@ -35,12 +42,22 @@ export default function RandomStuffs() {
                     onChange={handlePhoneNumberChange}
                 />
                 <p>Your phone number: {formatNumber(phoneNumber)}</p>
-                <input type="checkbox" id="checkbox" onChange={() => setAdvanceMode(!advanceMode)}/> 
+                <input
+                    type="checkbox"
+                    id="checkbox"
+                    onChange={() => setAdvanceMode(!advanceMode)}
+                />
                 <label>Advance Mode</label>
-                <input type="checkbox" id="checkbox" onChange={() => setAdvanceMode(!advanceMode)}/> 
+                <input
+                    type="checkbox"
+                    id="checkbox"
+                    onChange={() => setEnhance(!enhance)}
+                />
                 <label>Enhance</label>
             </div>
-            <button className="" onClick={() => confirmPhoneNumber()}>Submit</button>
+            <button className="" onClick={() => confirmPhoneNumber()}>
+                Submit
+            </button>
         </>
     );
 }
